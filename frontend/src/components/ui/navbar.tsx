b1cf8b7671fbaed159ddb11b3a46ca27aebd7c97
orbitzy <orbittergourd@gmail.com>
2025-02-15 17:44:05 +0800

    change nav

--- frontend/src/components/ui/navbar.tsx
+++ frontend/src/components/ui/navbar.tsx
@@ -4,7 +4,7 @@ import { Smartphone } from "lucide-react"
 const Navbar = () => {
   return (
     <header className="px-4 lg:px-6 h-20 fixed top-0 left-0 right-0 z-10 bg-white flex items-center justify-between max-w-7xl mx-auto w-full">
-      <Link className="flex items-center justify-center" href="#">
+      <Link className="flex items-center justify-center" href="/">
         <Smartphone className="h-6 w-6 text-blue-600" />
         <span className="ml-2 text-2xl font-bold text-gray-900">Wellquit</span>
       </Link>
@@ -15,8 +15,8 @@ const Navbar = () => {
         <Link className="text-sm font-medium hover:underline underline-offset-4" href="#testimonials">
           Testimonials
         </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#about">
-          About
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/feed">
+          Feed
         </Link>
       </nav>
     </header>
