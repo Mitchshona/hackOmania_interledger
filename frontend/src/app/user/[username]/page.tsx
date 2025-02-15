524ceb1c8c365e5155724c6683558fb3f3919903
orbitzy <orbittergourd@gmail.com>
2025-02-16 01:16:42 +0800

    updating landing page

--- frontend/src/app/user/[username]/page.tsx
+++ frontend/src/app/user/[username]/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/ui/navbar"
 
 export default function ProfilePage() {
   return (
-    <main className="container mx-auto px-4 pt-24 pb-8">
+    <main className="container mx-auto px-4 pt-8 pb-8">
       <ProfileInfo />
       <StreakCard streakCount={0} />
       <UserPosts />
